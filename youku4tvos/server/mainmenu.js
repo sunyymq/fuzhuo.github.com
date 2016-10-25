bb9577529bee1128d948dac28d30faa165796169
fuzhuo <fuzhuo233@gmail.com>
2016-10-25 16:12:03 +0800

    first edition

---
+++ youku4tvos/server/mainmenu.js
@@ -0,0 +1,142 @@
+const HOST="http://tv.api.3g.youku.com/";
+const IDS="pid=0ce22bfd5ef5d2c5&guid=12d60728bd267e3e0b6ab4d124d6c5f0&ngdid=357e71ee78debf7340d29408b88c85c4&ver=2.6.0&operator=T-Mobile_310260&network=WIFI&launcher=0";
+var mainDoc;
+
+var channels = {
+    "data":[
+        {"title":"电影", "channel_id":96, "page":1},
+        {"title":"电视剧", "channel_id":97, "page":1},
+        {"title":"动漫", "channel_id":100, "page":1},
+        {"title":"综艺", "channel_id":85, "page":1},
+        {"title":"纪录片", "channel_id":84, "page":1},
+        //{"title":"教育", "channel_id":87, "page":1},
+        {"title":"历史", "channel_id":-1},
+        {"title":"搜索", "channel_id":-1}
+    ]
+};
+
+var getCategoryPageWithChannelID = function(index, callback) {
+    let channel_id = channels['data'][index]['channel_id'];
+    let page = channels['data'][index]['page'];
+    let page_size = 58;
+    var url = `${HOST}layout/smarttv/item_list?${IDS}&cid=${channel_id}&pz=${page_size}&pg=${page}&filter=`;
+    console.log("url:"+url);
+    getHTTP(url, function(content){
+        //console.log("content:"+content);
+        let data=JSON.parse(content);
+        var docText = `
+        <document>
+           <stackTemplate>
+              <banner>
+                 <title><![CDATA[${channels['data'][index]['title']}]]></title>
+              </banner>
+              <collectionList>
+                 <grid>
+                    <section>`;
+        for(var values of data['results']) {
+            docText += `
+                       <lockup onselect="showSeries('${values['showid']}')">
+                          <img src="${values['show_vthumburl_hd']}" width="250" height="376" />
+                          <title><![CDATA[${values['showname']}]]></title>`;
+            if (values['paid']!=0) {
+                docText += `
+                          <overlay style="padding: 0">
+                              <title style="background-color: rgba(0,0,0,0.6); color: #FFFFFF; text-align: center; width: 300">VIP</title>
+                          </overlay>`;
+            }
+            docText += `
+                       </lockup>`;
+        }
+        if (page>1) {
+            docText += `
+                        <lockup onselect="replacePageContent(${index},${page-1})">
+                          <img src="http://fuzhuo.qiniudn.com/prev.png" width="250" height="376" />
+                          <title>第${page-1}页</title>
+                        </lockup>`;
+        }
+        if (page*page_size < parseInt(data['total'])) {
+            docText += `
+                        <lockup onselect="replacePageContent(${index},${page+1})">
+                          <img src="http://fuzhuo.qiniudn.com/next.png" width="250" height="376" />
+                          <title>第${page+1}页</title>
+                        </lockup>`;
+        }
+        docText += `
+                    </section>
+                 </grid>
+              </collectionList>
+           </stackTemplate>
+        </document>`;
+        //console.log("docText:"+docText);
+        callback((new DOMParser).parseFromString(docText, "application/xml"));
+    });
+}
+
+var templateFuncs = [
+    getCategoryPageWithChannelID,
+    getCategoryPageWithChannelID,
+    getCategoryPageWithChannelID,
+    getCategoryPageWithChannelID,
+    getCategoryPageWithChannelID,
+    //getCategoryPageWithChannelID,
+    history_func,
+    search
+];
+
+function replacePageContent(index, page) {
+    channels['data'][index]['page']=page;
+    console.log("replacePageContent: index:"+index+",page:"+page);
+    getCategoryPageWithChannelID(index, function(doc) {
+            const Elem = mainDoc.getElementsByTagName("menuBar").item(0);
+            const childs=Elem.childNodes;
+            const menuEle = childs.item(index);
+            const feature = menuEle.parentNode.getFeature("MenuBarDocument");
+            feature.setDocument(doc, menuEle);
+        });
+}
+
+var mainmenuUI = function() {
+    var tmp = `<?xml version="1.0" encoding="UTF-8" ?>
+		<document>
+			<menuBarTemplate>
+				<menuBar>`;
+    let i=0;
+    for(var d of channels['data']) {
+        tmp+=`
+                    <menuItem index="${i}">
+                        <title>${d['title']}</title>
+                    </menuItem>`;
+        i++;
+    }
+	tmp +=`
+                </menuBar>
+			</menuBarTemplate>
+		</document>`;
+    console.log("mainmenu: "+tmp);
+    return (new DOMParser).parseFromString(tmp, "application/xml");
+}
+
+function showMainMenu(func) {
+    mainDoc = mainmenuUI();
+    const Elem = mainDoc.getElementsByTagName("menuBar").item(0);
+    Elem.addEventListener("select", (event)=> {
+            const target = event.target;
+            const index = parseInt(target.getAttribute("index"));
+            const ele = event.target.parentNode;
+            const feature = ele.getFeature("MenuBarDocument");
+            if (!feature.getDocument(event.target)) {
+                templateFuncs[index](index, function(doc, loading) {
+                    feature.setDocument(doc, event.target);
+                });
+            }
+        });
+    navigationDocument.replaceDocument(mainDoc, getActiveDocument());
+}
+
+function refreshHistory() {
+    history_func(5, function(doc, loading) {
+            const Elem = mainDoc.getElementsByTagName("menuItem").item(5);
+        const feature = Elem.parentNode.getFeature("MenuBarDocument");
+        feature.setDocument(doc, Elem);
+    });
+}
---
+++ youku4tvos/server/search.js
@@ -0,0 +1,263 @@
+var search = function (index, callback) {
+    var tmp = `<?xml version="1.0" encoding="UTF-8" ?>
+    <document>
+    <searchTemplate>
+        <searchField>Search</searchField>
+        <collectionList>
+        </collectionList>
+    </searchTemplate>
+    </document>`;
+    var doc = (new DOMParser).parseFromString(tmp, "application/xml");
+    var searchField = doc.getElementsByTagName("searchField").item(0);
+    var keyboard = searchField.getFeature("Keyboard");
+    keyboard.onTextChange = function() {
+        var searchText = keyboard.text;
+        updateSearch(doc, searchText);
+    }
+    updateSearch(doc, "");
+    doc.addEventListener("select", (event)=> {
+                         const target = event.target;
+                         const attr = target.getAttribute("type");
+                         if (attr) {
+                         const title = target.getElementsByTagName("title").item(0);
+                         console.log("target:"+title.textContent);
+                         showSearchResultsUI(title.textContent);
+                         }
+                         });
+    callback(doc);
+}
+
+function updateSearch(doc, keyword) {
+    if (keyword.length > 0) {
+        let url = `${HOST}openapi-wireless/keywords/suggest?${IDS}&keywords=${keyword}`;
+        console.log("url: "+url);
+        getHTTP(url, function(datastr){
+            var data = JSON.parse(datastr);
+            var dom = doc.implementation;
+            var lsParser = dom.createLSParser(1, null);
+            var lsInput = dom.createLSInput();
+            var text = `
+                <grid>
+                    <header>
+                        <title>请选择联想关键字</title>
+                    </header>`;
+                text += `
+                    <section>`;
+                text += `
+                        <lockup type="lockup">
+                            <img src="" width="300" height="45"/>
+                            <overlay style="padding: 0">
+                                <title style="background-color: rgba(0,0,0,0.6); color: #FFFFFF; text-align: center; width: 300">${keyword}</title>
+                            </overlay>
+                        </lockup>`;
+                for (var value of data['results']) {
+                text += `
+                        <lockup type="lockup">
+                            <img src="" width="300" height="45"/>
+                            <overlay style="padding: 0">
+                                <title style="background-color: rgba(0,0,0,0.6); color: #FFFFFF; text-align: center; width: 300">${value['keyword']}</title>
+                            </overlay>
+                        </lockup>`;
+                }
+                text += `
+                    </section>`;
+                text += `
+                </grid>`;
+                console.log("text: "+text);
+            lsInput.stringData = text;
+            lsParser.parseWithContext(lsInput, doc.getElementsByTagName("collectionList").item(0), 2);
+        });
+    } else {
+        let url = `${HOST}openapi-wireless/keywords/recommend?${IDS}`;
+        console.log("url: "+url);
+        getHTTP(url, function(datastr){
+            var data = JSON.parse(datastr);
+            var dom = doc.implementation;
+            var lsParser = dom.createLSParser(1, null);
+            var lsInput = dom.createLSInput();
+            var text = `
+                <grid>
+                    <header>
+                        <title>大家都在搜</title>
+                    </header>`;
+                text += `
+                    <section>`;
+                for (var value of data['results']) {
+                text += `
+                        <lockup type="lockup">
+                            <img src="" width="300" height="45"/>
+                            <overlay style="padding: 0">
+                                <title style="background-color: rgba(0,0,0,0.6); color: #FFFFFF; text-align: center; width: 300">${value['title']}</title>
+                            </overlay>
+                        </lockup>`;
+                }
+                text += `
+                    </section>`;
+                text += `
+                </grid>`;
+                console.log("text: "+text);
+            lsInput.stringData = text;
+            lsParser.parseWithContext(lsInput, doc.getElementsByTagName("collectionList").item(0), 2);
+        });
+    }
+}
+
+function showSearchResultsUI(text, pg=1, pz=20, replace=false) {
+    var loading = createLoadingDocument('查询中...');
+    if (replace) {
+        navigationDocument.replaceDocument(loading, getActiveDocument());
+    } else {
+        navigationDocument.pushDocument(loading);
+    }
+    showSearchResults(text, pg, pz, function(doc){
+        navigationDocument.replaceDocument(doc, loading);
+    });
+}
+
+function showSearchResults(text, pg, pz, callback) {
+    var url = `${HOST}layout/smarttv/showsearch?copyright_status=1&video_type=1&keyword=${encodeURI(text)}&${IDS}`;
+    var video_url = `${HOST}openapi-wireless/videos/search/${encodeURI(text)}?${IDS}&pg=${pg}&pz=${pz}`;
+    console.log("series url: "+url);
+    console.log("video url: "+video_url);
+    getHTTP(url, function(datastr){
+        getHTTP(video_url, function(videostr){
+        console.log("docText:"+datastr);
+        const data = JSON.parse(datastr);
+        const video_data = JSON.parse(videostr);
+        var docText = `
+        <document>
+           <stackTemplate>
+              <banner>
+                <title>『${text}』搜索结果</title>
+              </banner>
+              <collectionList>`;
+        if (pg==1 && data['results'].length > 0) {
+        docText += `
+              <separator style="margin: 40 0 20">
+                 <title>剧集</title>
+              </separator>`;
+        docText+= `
+                 <grid>
+                    <section>`;
+            for( var value of data['results']) {
+        docText+= `
+                       <lockup onselect="showSeries('${value['showid']}')">
+                          <img src="${value['show_vthumburl_hd']}" width="250" height="376" />
+                          <title><![CDATA[${value['showname']}]]></title>
+                       </lockup>`;
+            }
+        docText += `
+                    </section>
+                 </grid>`;
+        }
+        let total = video_data['total'];
+        docText += `
+              <separator style="margin: 40 0 20">
+                <title>视频(共${total}个结果)</title>
+              </separator>`;
+        docText+= `
+                 <grid>
+                    <section>`;
+        let idx = 0;
+        for( var value of video_data['results']) {
+        docText+= `
+                       <lockup idx="${idx}">
+                          <img src="${value['img']}" width="250" height="376" />
+                          <title><![CDATA[${value['title']}]]></title>
+                       </lockup>`;
+            idx++;
+        }
+        if (total>0 && pg>1) {
+        docText+= `
+                       <lockup idx="-2">
+                          <img src="http://fuzhuo.qiniudn.com/prev.png" width="250" height="376" />
+                          <title>上一页</title>
+                       </lockup>`;
+        }
+        if (total>0 && pg*pz<total) {
+        docText+= `
+                       <lockup idx="-1">
+                          <img src="http://fuzhuo.qiniudn.com/next.png" width="250" height="376" />
+                          <title>下一页</title>
+                       </lockup>`;
+        }
+        docText += `
+                    </section>
+                 </grid>`;
+        docText += `
+              </collectionList>
+           </stackTemplate>
+        </document>`;
+        console.log("docText: "+docText);
+        var doc = (new DOMParser).parseFromString(docText, "application/xml")
+        doc.addEventListener("select", (event)=> {
+                             const target = event.target;
+                             const idx = target.getAttribute("idx");
+                             if (idx >=0) {
+                             playSearchVideo(video_data, idx);
+                             } else if (idx == -2) {
+                             showSearchResultsUI(text, pg-1, 20, true);
+                             } else if (idx == -1) {
+                             showSearchResultsUI(text, pg+1, 20, true);
+                             }
+                             });
+        callback(doc);
+    })
+    });
+}
+
+function playSearchVideo(data, index) {
+    var video_id = data['results'][index]['videoid'];
+    console.log("playsearch video: "+index+" ,videoid:"+video_id);
+    var uri = 'http://play.youku.com/play/get.json?vid='+video_id+'&ct=12';
+    getHTTP(uri, function(text) {
+        var movdat = JSON.parse(text);
+        var oip = movdat['data']['security']['ip'];
+        var epdata = movdat['data']['security']['encrypt_string'];
+        
+        var templateXHR = new XMLHttpRequest();
+        var url = baseURL+"ykdecode.php?vid="+video_id+"&oip="+encodeURI(oip)+"&epdata="+encodeURI(epdata);
+        console.log("request m3u8 by url: " + url);
+        templateXHR.responseType = "document";
+        templateXHR.addEventListener("load", function() {
+            var m3u8=templateXHR.responseText;
+            playSearchVideoWithM3U8(data, index, m3u8);
+        }, false);
+        templateXHR.open("GET", url, true);
+        templateXHR.send();
+    });
+}
+function playSearchVideoWithM3U8(data, index, m3u8) {
+    console.log("request m3u8 by url: " + m3u8);
+    var vid = data['results'][index]['videoid'];
+    var video = new MediaItem('video', m3u8);
+    var progress_str = sessionStorage.getItem("progress");
+    if (typeof progress_str === 'undefined' || progress_str == "undefined") {
+        progressData = {};
+    } else {
+        var progressData = JSON.parse(progress_str);
+        if (progressData && progressData[vid]) {
+            video.resumeTime = progressData[vid];
+        }
+    }
+    video.artworkImageURL = data['results'][index]['img'];
+    video.title = data['results'][index]['title'];
+    var videoList = new Playlist();
+    videoList.push(video);
+    var myPlayer = new Player();
+    myPlayer.playlist = videoList;
+    myPlayer.addEventListener('timeDidChange', function(listener,extraInfo) {
+                              progressData[vid] = listener.time;
+                              },{interval: 1});
+    myPlayer.addEventListener('stateDidChange', function(listener, extraInfo) {
+                              console.log("progress: "+JSON.stringify(progressData));
+                              if (listener.state == 'end') {
+                                  console.log('Play end');
+                                  sessionStorage.setItem("progress", JSON.stringify(progressData));
+                              } else if (listener.state == 'paused') {
+                                  console.log('Play paused, update history time');
+                                  sessionStorage.setItem("progress", JSON.stringify(progressData));
+                              }
+    },{});
+    myPlayer.play();
+}
