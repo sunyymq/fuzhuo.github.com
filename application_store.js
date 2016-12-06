312a5c2faab3c9472822ff87be4eb34a9bc85711
fuzhuo <fuzhuo233@gmail.com>
2016-12-06 10:44:16 +0800

    update home page for store version

--- application_store.js
+++ application_store.js
@@ -69,8 +69,8 @@ function appStart(baseURL) {
     evaluateScripts(scripts, function(scriptsAreLoaded) {
         if (scriptsAreLoaded) {
             console.log("scripts are loaded");
-            //showHomePage();
-            showMainMenu(loadingDocument);
+            showHomePage();
+            //showMainMenu(loadingDocument);
         } else {
             const alertDocument = createEvalErrorAlertDocument();
             navigationDocument.replaceDocument(alertDocument, loadingDocument);
